0fd5f1ed0024ac0dadfc28199fa31b992e123be5
jakesig <jsigman04@gmail.com>
2019-10-07 12:48:21 -0400

    Serializable - Update

--- Serializable/src/Dog.java
+++ Serializable/src/Dog.java
@@ -1,17 +1,26 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Dog implements Serializable {
     private String name;
     private double weight;
-    private int age;
+    private transient int age;
 
     public Dog() {}
+
+    public Dog(String name, double weight, int age) {
+        this.name = name;
+        this.weight = weight;
+        this.age = age;
+    }
 
     public static void main(String[] args) throws IOException {
         Dog d = new Dog();
         d.setName("Rover");;
         d.setAge(3);
         d.setWeight(47.6);
+        System.out.println("Original Dog:"+d);
         try {
             File dog = new File("H:\\CSAB Library\\Quarter 1\\Serializable Project\\dog.txt");
             FileOutputStream out = new FileOutputStream(dog);
@@ -20,12 +29,25 @@ public class Dog implements Serializable {
             obj.close();
         }
         catch (FileNotFoundException e) {
-            e.printStackTrace();
+        }
+        ArrayList<Dog> pack = new ArrayList<Dog>(Arrays.asList(
+                new Dog("Ron",30.4,5),
+                new Dog("Jon", 40.6, 6),
+                new Dog("Larry",32.5,15)));
+        System.out.println("Original List:"+pack);
+        try {
+            File dogList = new File("H:\\CSAB Library\\Quarter 1\\Serializable Project\\dogList.txt");
+            FileOutputStream outList = new FileOutputStream(dogList);
+            ObjectOutputStream objList = new ObjectOutputStream(outList);
+            objList.writeObject(pack);
+            objList.close();
+        }
+        catch (FileNotFoundException e) {
         }
     }
 
     public String toString() {
-        return "Name: " + this.name + "\nAge: " + this.age + "\nWeight: " + this.weight;
+        return "\nName: " + this.name + "\nAge: " + this.age + "\nWeight: " + this.weight;
     }
 
     public void setName(String newName) {
--- Serializable/src/DogObtainer.java
+++ Serializable/src/DogObtainer.java
@@ -1,21 +1,31 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class DogObtainer {
     public static void main(String[] args) {
+        //serialize a dog.
         Dog d;
         try {
             File otherDog = new File("H:\\CSAB Library\\Quarter 1\\Serializable Project\\dog.txt");
             FileInputStream in = new FileInputStream(otherDog);
             ObjectInputStream obj = new ObjectInputStream(in);
             d = (Dog)obj.readObject();
-            System.out.println(d);
+            System.out.println("New Dog:"+d);
             obj.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+
+        }
+        //serialize an arrayList of dogs.
+        ArrayList<Dog> otherPack;
+        try {
+            File otherDogList = new File("H:\\CSAB Library\\Quarter 1\\Serializable Project\\dogList.txt");
+            FileInputStream inList = new FileInputStream(otherDogList);
+            ObjectInputStream objList = new ObjectInputStream(inList);
+            otherPack = (ArrayList)objList.readObject();
+            System.out.println("New List:"+otherPack);
+            objList.close();
+        } catch (Exception e) {
+
         }
     }
 }
