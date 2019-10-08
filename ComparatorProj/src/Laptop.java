381656a3019c4653be8209f7c9edd71aa60f1bb3
jakesig <jsigman04@gmail.com>
2019-10-08 12:27:12 -0400

    Comparator - Finished

--- ComparatorProj/src/Laptop.java
+++ ComparatorProj/src/Laptop.java
@@ -1,2 +1,42 @@
 public class Laptop {
+    private String brand;
+    private int memory;
+    private double price;
+
+    public Laptop(String brand, int memory, double price) {
+        this.brand = brand;
+        this.memory = memory;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return  "Brand:  " + brand +
+                "\tMemory:  " + memory +
+                "\tPrice:  " + price;
+    }
 }
