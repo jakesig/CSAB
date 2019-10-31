52273f65d32f99f4bd65d84160c69d3de87096cb
jakesig <jsigman04@gmail.com>
2019-10-31 13:46:20 -0400

    Reusable Dog

---
+++ Reusable Dog/src/dog/Poodle.java
@@ -0,0 +1,12 @@
+package dog;
+
+/**
+ * Write a description of class dog.Poodle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Poodle extends Dog
+{
+   
+}
