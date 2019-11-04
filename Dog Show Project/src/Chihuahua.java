76ab4c0a4acccba8bac9f547b1081d8daa2a3cc6
jakesig <jsigman04@gmail.com>
2019-11-04 07:39:55 -0500

    Dog Show Creation

---
+++ Dog Show Project/src/Chihuahua.java
@@ -0,0 +1,15 @@
+package dog;
+
+/**
+ * Write a description of class dog.Chihuahua here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Chihuahua extends Dog
+{
+    @Override public void speak() {
+        System.out.println("Woof!");
+        System.out.println("Woof!\n");
+    }
+}
---
+++ Dog Show Project/src/Poodle.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class dog.Poodle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Poodle extends Dog
+{
+   
+}
