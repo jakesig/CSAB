2ec22f4c516545abbdf245fe8916b5dac65ad302
jakesig <jsigman04@gmail.com>
2019-11-06 08:40:49 -0500

    DogShow Revised

--- Dog Show Project/src/DogShow.java
+++ Dog Show Project/src/DogShow.java
@@ -3,38 +3,51 @@ import java.util.*;
 public class DogShow{
     private static ArrayList<ShowDog> show;
     private static Random generator = new Random();
-
+    public static final int DEFAULT = 100;
+    public static final int MEDALS = 1000;
+    public static final int TYPE = 4;
     public DogShow() {
         show = new ArrayList<ShowDog>();
+    }
+    private void randomDogs() {
+        show.clear();
+        int rand;
+        for (int i = 0; i < DEFAULT ; i++) {
+            rand = generator.nextInt(TYPE);
+            if (rand==0)
+                show.add(new Poodle("Poodle "+generator.nextInt(MEDALS), generator.nextInt(9), generator.nextDouble()*DEFAULT));
+            if (rand==1)
+                show.add(new Poodle("Basenji "+generator.nextInt(MEDALS), generator.nextInt(9), generator.nextDouble()*DEFAULT));
+            if (rand==2)
+                show.add(new Poodle("Beagle "+generator.nextInt(MEDALS), generator.nextInt(9),generator.nextDouble()*DEFAULT));
+            if (rand==3)
+                show.add(new Poodle("Chihuahua "+generator.nextInt(MEDALS), generator.nextInt(9),generator.nextDouble()*DEFAULT));
+        }
+    }
+    private void sortDogs() {
         Comparator<ShowDog> comp = new Comparator<ShowDog>(){
             public int compare(ShowDog d1, ShowDog d2) {
                 return d2.getMedals()-d1.getMedals();
             }
         };
-        int rand;
-        for (int i = 0; i < 100 ; i++) {
-            rand = generator.nextInt(4);
-            if (rand==0)
-                show.add(new Poodle("Poodle "+generator.nextInt(1000), generator.nextInt(9), generator.nextDouble()*100));
-            if (rand==1)
-                show.add(new Poodle("Basenji "+generator.nextInt(1000), generator.nextInt(9), generator.nextDouble()*100));
-            if (rand==2)
-                show.add(new Poodle("Beagle "+generator.nextInt(1000), generator.nextInt(9),generator.nextDouble()*100));
-            if (rand==3)
-                show.add(new Poodle("Chihuahua "+generator.nextInt(1000), generator.nextInt(9),generator.nextDouble()*100));
-        }
-        for (int i = 0; i < 1000 ; i++) {
-            show.get(generator.nextInt(100)).awardMedal();
-        }
         Collections.sort(show, comp);
     }
-
+    private void awardMedals() {
+        for (int i = 0; i < MEDALS; i++) {
+            show.get(generator.nextInt(DEFAULT)).awardMedal();
+        }
+    }
+    public void showTime() {
+        randomDogs();
+        awardMedals();
+        sortDogs();
+        System.out.println("First:\n"+show.get(0));
+        System.out.println("Second:\n"+show.get(1));
+        System.out.println("Third:\n"+show.get(2));
+    }
     public static void main(String[] args) {
         DogShow ds = new DogShow();
         //System.out.println("Winners:");
-        System.out.println("First:\n"+ds.show.get(0));
-        System.out.println("Second:\n"+ds.show.get(1));
-        System.out.println("Third:\n"+ds.show.get(2));
-
+        ds.showTime();
     }
 }
