9f0f8d86be83674ae1d0a752ed9fa03f5170bd88
jsigm <jsigman04@gmail.com>
2019-11-07 10:05:41 -0500

    BattleShip GUI

--- Battleship Lab/src/Controller.java
+++ Battleship Lab/src/Controller.java
@@ -2,14 +2,17 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 public class Controller extends Application implements EventHandler<ActionEvent> {
     private GridPane pane;
     private Scene scene;
+    private Button[][] grid;
     @Override public void start(Stage stage) {
         pane = new GridPane();
-        scene = new Scene(pane, 200,300);
+        scene = new Scene(pane, 500,500);
+        setButtons();
         stage.setScene(scene);
         stage.show();
     }
@@ -20,5 +23,17 @@ public class Controller extends Application implements EventHandler<ActionEvent>
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void setButtons() {
+        grid = new Button[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                grid[i][j] = new Button("");
+                grid[i][j].setPrefSize(50,50);
+                pane.add(grid[i][j],i,j);
+                grid[i][j].setOnAction(this);
+            }
+        }
     }
 }
