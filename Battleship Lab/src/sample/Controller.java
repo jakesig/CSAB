bf4f95532ef85266e12f5f06bb37a41eeb5b6167
jsigm <jsigman04@gmail.com>
2019-11-17 17:47:55 -0500

    90% chance of it functioning properly.

--- Battleship Lab/src/sample/Controller.java
+++ Battleship Lab/src/sample/Controller.java
@@ -12,31 +12,35 @@ import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.math.BigInteger;
 import java.util.*;
 
 public class Controller extends Application implements EventHandler<ActionEvent> {
-    private Stage primary;
     private GridPane pane;
-    private Scene scene;
-    private Scene finished;
     private Button[][] grid;
     private Board myBoard;
     private static final int GRIDSIZE = 10;
     private ArrayList<Coordinate> guessed = new ArrayList<Coordinate>();
     private int sunkShips;
+    private boolean done;
     @Override public void start(Stage stage) {
         myBoard = new Board();
         myBoard.setShips();
         pane = new GridPane();
-        scene = new Scene(pane, GRIDSIZE*50,GRIDSIZE*50);
+        Scene scene = new Scene(pane, GRIDSIZE * 50, GRIDSIZE * 50);
         stage.setTitle("BattleShip");
         setButtons();
         stage.setScene(scene);
         stage.show();
     }
     @Override public void handle(ActionEvent event) {
+        Text text=new Text();
+        text.setFont(Font.font("", FontWeight.BOLD, FontPosture.REGULAR, 15));
+        text.setX(50);
+        text.setY(30);
+        Group root = new Group(text);
+        Scene finished = new Scene(root, GRIDSIZE * 25, GRIDSIZE * 10);
+        Stage primary = new Stage();
+        primary.setScene(finished);
         Coordinate coord = Coordinate.newCoord(myBoard, 10, 10);
         boolean correct = false;
         String sunk = "";
@@ -65,23 +69,18 @@ public class Controller extends Application implements EventHandler<ActionEvent>
             grid[coord.getX()][coord.getY()].setStyle("-fx-background-color: #ffffff; ");
         }
         sunk = myBoard.sunk(guessed, grid);
+        if (done) {
+            System.exit(0);
+        }
         if (!sunk.equals("None")) {
             sunkShips++;
-            System.out.println("Ship sunk: " + sunk);
+            text.setText("You sunk the "+sunk+"!");
+            primary.show();
         }
         if (sunkShips==5) {
-            //System.out.println("You sunk all the ships! Exiting in 5 seconds.");
-            Text text = new Text();
-            text.setText("You sunk all the ships!\n\n\n\n\n\n\n");
-            text.setFont(Font.font("", FontWeight.BOLD, FontPosture.REGULAR, 30));
-            text.setX(50);
-            text.setY(50);
-            Group root = new Group(text);
-            finished = new Scene(root, GRIDSIZE*50, GRIDSIZE*50);
-            primary = new Stage();
-            primary.setScene(finished);
+            text.setText("You sunk all the ships!\n\nClick anywhere on\nthe grid to exit.");
             primary.show();
-            //System.exit(0);
+            done = true;
         }
     }
     private void setButtons() {
