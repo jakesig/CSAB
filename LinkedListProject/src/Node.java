0056bc342c8c1290df04831af285b95c95ea57fb
jsigm <jsigman04@gmail.com>
2019-12-04 21:26:27 -0500

    Stack Finished

--- LinkedListProject/src/Node.java
+++ LinkedListProject/src/Node.java
@@ -1,54 +1,99 @@
+import java.util.ArrayList;
+
 public class Node {
-    public String data;
-    public int otherData;
+    public Object data;
     public Node next;
-    public Node(String str, int num) {
-        this.data = str;
-        this.otherData = num;
-    }
-    public void display() {
-        System.out.println("String: "+data+"\nInteger: "+otherData);
+    public Node(Object obj) {
+        this.data = obj;
     }
     @Override public String toString() {
-        return data+" | "+otherData;
+        return ""+data;
     }
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        list.insertFirstNode("A", 3);
-        list.insertFirstNode("B", 7);
-        list.insertFirstNode("C", 9);
-        list.insertFirstNode("D", 7);
-        //list.removeNode(7);
+        list.add(3);
+        list.add(7);
+        list.add(5);
+        list.add(7);
+        list.add(6);
         System.out.println(list);
-        list.removeLast(list.firstLink);
         System.out.println(list);
     }
 }
 
 class LinkedList {
     public Node firstLink;
-    public int size;
+    private int size;
     public LinkedList() {
         firstLink = null;
     }
     public boolean isEmpty() {
         return (firstLink==null);
     }
-    public void insertFirstNode(String str, int num) {
-        Node newNode = new Node(str, num);
+    public int size() {
+        return size;
+    }
+    public ArrayList<Node> navigate(int index) {
+        ArrayList<Node> arr = new ArrayList<Node>();
+        int i = 0;
+        if (index>=this.size) {
+            throw (new ArrayIndexOutOfBoundsException());
+        }
+        Node current = firstLink.next;
+        Node previous = firstLink;
+        while(i<index-1) {
+            previous = current;
+            current = current.next;
+            i++;
+        }
+        arr.add(previous);
+        arr.add(current);
+        return arr;
+    }
+    public void add(Object obj) {
+        Node newNode = new Node(obj);
         newNode.next = firstLink;
         firstLink = newNode;
         size++;
     }
-    public Node removeFirst() {
-        Node nodeRef = firstLink;
-        if(!isEmpty()) {
-            firstLink = firstLink.next;
+    public Node get(int index) {
+        if (this.isEmpty()) {
+            return null;
         }
-        else {
-            System.out.println("Empty LinkedList");
+        if (index==0) {
+            return firstLink;
         }
-        return nodeRef;
+        ArrayList<Node> toGet = this.navigate(index);
+        return toGet.get(1);
+    }
+    public void set(Object obj, int index) {
+        if (this.isEmpty()) {
+            return;
+        }
+        if (index==0) {
+            Node temp = firstLink.next;
+            firstLink = new Node(obj);
+            firstLink.next = temp;
+            return;
+        }
+        ArrayList<Node> alter = this.navigate(index);
+        alter.get(0).next = new Node(obj);
+        alter.get(0).next.next = alter.get(1).next;
+    }
+    public void insert(Object obj, int index) {
+        if (this.isEmpty()) {
+            return;
+        }
+        if (index==0) {
+            Node temp = firstLink;
+            firstLink = new Node(obj);
+            firstLink.next = temp;
+            return;
+        }
+        ArrayList<Node> alter = this.navigate(index);
+        alter.get(0).next = new Node(obj);
+        alter.get(0).next.next = alter.get(1);
+        size++;
     }
     @Override public String toString() {
         String s = "";
@@ -61,27 +106,13 @@ class LinkedList {
         }
         return s;
     }
-    public Node find(String str) {
-        Node node = firstLink;
-        if (!isEmpty()) {
-            while (!node.data.equals(str)) {
-                if (node.next == null) {
-                    return null;
-                }
-                else {
-                    node = node.next;
-                }
-            }
+    public Node remove(Object obj) {
+        if (this.isEmpty()) {
+            return null;
         }
-        else {
-            System.out.println("Empty Linked List");
-        }
-        return node;
-    }
-    public Node removeNode(int num) {
         Node current = firstLink;
         Node previous = firstLink;
-        while(current.otherData!=num) {
+        while(current.data!=obj) {
             if (current.next==null) {
                 return null;
             }
@@ -98,16 +129,15 @@ class LinkedList {
         }
         return current;
     }
-    public void removeLast(Node front) {
-        if (front==null) {
+    public void remove(int index) {
+        if (this.isEmpty()) {
             return;
         }
-        Node temp = front.next;
-        Node prev = front;
-        while (temp.next!=null) {
-            prev = temp;
-            temp = temp.next;
+        if (index ==0) {
+            firstLink=firstLink.next;
+            return;
         }
-        prev.next=null;
+        ArrayList<Node> alter = this.navigate(index);
+        alter.get(0).next = alter.get(1).next;
     }
 }
---
+++ StackProject/src/Stack.java
@@ -0,0 +1,61 @@
+public class Stack {
+    private Object[] arr;
+    private int size;
+    private static final int MAXSIZE = 100;
+    public Stack() {
+        arr = new Object[MAXSIZE];
+        size = 0;
+    }
+    public int size() {
+        return size;
+    }
+    @Override public String toString() {
+        String s = "";
+        for (int i = this.size()-1; i >=0; i--) {
+            s+=arr[i]+"\n";
+        }
+        return s;
+    }
+    public boolean isEmpty() {
+        return (size==0);
+    }
+    public boolean isFull() {
+        return (size==MAXSIZE);
+    }
+    public Object peek() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        return arr[size-1];
+    }
+    public Object pop() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        Object toReturn = arr[size-1];
+        arr[size-1] = null;
+        size--;
+        return toReturn;
+    }
+    public void push(Object obj) {
+        if (this.isFull()) {
+            System.out.println("Maximum size reached. Can't push.");
+            return;
+        }
+        arr[size] = obj;
+        size++;
+    }
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            arr[i] = null;
+        }
+        size = 0;
+    }
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(5);
+        s.push(3);
+    }
+}
