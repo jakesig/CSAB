677d5df967375ce23d00e0806c6aa60e2ca90e83
jsigm <jsigman04@gmail.com>
2020-02-27 20:50:41 -0500

    Permute String Using Stack//Graph Extra?

--- Graph Project/src/Graph.java
+++ Graph Project/src/Graph.java
@@ -10,11 +10,28 @@ public class Graph {
         graph[from][to]=true;
         graph[to][from]=true;
     }
-    public void mis(Graph g) {
+    public Set<Integer> mis(Graph g) {
         Set<Integer> set1 = new HashSet<Integer>();
         Set<Integer> set2 = new HashSet<Integer>();
         for (int i = 0; i < graph[0].length; i++) {
             set1.add(i);
         }
+        int var = set1.get();
+        set1.remove(var);
+        set2.add(var);
+        for (int i = 0;  < set1.size(); ++) {
+            if (graph[var][i]==false) {
+                set2.add(graph[var][i]);
+                var = set2.remove(graph[var][i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph(4);
+        g.addEdge(0,3);
+        g.addEdge(0,1);
+        g.addEdge(0,2);
+        System.out.println(mis(g));
     }
 }
---
+++ StringStack/src/StringStack.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Stack;
+
+public class StringStack {
+    public Set<String> permute(String word) {
+        String constant = "-";
+        String first = constant+word;
+        Set<String> set = new HashSet<String>();
+        Stack<String> stack = new Stack<String>();
+        stack.push(first);
+        while(!stack.isEmpty()) {
+            String temp = stack.pop();
+            if (temp.endsWith(constant)) {
+                set.add(temp.substring(0, temp.length()-1)+"\n");
+            }
+            else {
+                String[] splitUp = temp.split(constant);
+                for (int i = 0; i < splitUp[1].length(); i++) {
+                    stack.push(splitUp[0]+splitUp[1].charAt(i)+constant+splitUp[1].substring(0,i)+splitUp[1].substring(i+1));
+                }
+            }
+        }
+        return set;
+    }
+    public static void main(String[] args) {
+        StringStack ss = new StringStack();
+        Set<String> set = ss.permute("meet");
+        for (String s : set) {
+            System.out.print(s);
+        }
+    }
+}
