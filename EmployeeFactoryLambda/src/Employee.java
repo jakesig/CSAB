15a6b19d50a92dec2a6bb152a2c4602080bdac27
jsigm <jsigman04@gmail.com>
2020-06-09 23:46:02 -0400

    AVLTree and EmployeeFactoryLambda

--- EmployeeFactoryLambda/src/Employee.java
+++ EmployeeFactoryLambda/src/Employee.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Employee {
+    private String name;
+    public Employee() {
+        System.out.println("Empty Constructor");
+    }
+    public Employee(String name) {
+        System.out.println("Constructor with name");
+        this.name = name;
+    }
+    @Override public String toString() {
+        return "Name:" + name;
+    }
+    public static void main(String[] args) {
+        EmployeeFactory ef = Employee::new;
+        System.out.println("Constructor not called yet.");
+        Employee x = ef.get();
+        System.out.println(x);
+        EmployeeFactoryName efn = Employee::new;
+        System.out.println("New constructor about to be called");
+        Employee y = efn.get("Bob");
+        System.out.println(y);
+    }
 }
